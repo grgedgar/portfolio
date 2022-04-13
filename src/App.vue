996f9b0a87856048bf134481ff8972dcbbfbdfb2
Edgar Grygorian <e.hr00000@gmail.com>
2022-04-13 20:12:39 +0200

    ver.1.2

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,17 @@
       <span>Translate from / Перекласти з</span>
       <select id="selectedSourceLanguage" @change="selectSourceLanguage()">
       <option value="auto">Auto detect</option>
+      <option v-for="(value, name) in languagesList" :value="value" :key="value">{{name}}</option>
       </select>
       &nbsp; &nbsp;
       <span>Translate to / Перекласти на</span>
       <select id="selectedTargetLanguage" @change="selectTargetLanguage()">
-        <option value="uk">uk</option>
+        <option v-for="(value, name) in languagesList" :value="value" :key="value">{{name}}</option>
       </select>
     </div>
     <div class="container">
-      <textarea v-model="sourceText" placeholder="Введіть текст для перекладу" type='text' ref='sourceText' defaultValue="" v-on:keyup="detectSourceLanguage"></textarea>
-      <div class="translated-text-container">{{translatedText}}</div>
+      <textarea class="source-text-container" v-model="sourceText" placeholder="Введіть текст для перекладу" type='text' ref='sourceText' defaultValue="" v-on:keyup="detectSourceLanguage"></textarea>
+      <div id="targetText" class="target-text-container">{{translatedText}}</div>
     </div>
     <div class="container">
       <div id="detected-language"></div>
@@ -27,21 +28,22 @@
 export default {
   name: 'app',
   mounted() {
-    this.getLanguagesList();
+
   },
   methods: {
     selectSourceLanguage() {
       const e = document.getElementById('selectedSourceLanguage');
-      this.selectedSourceLanguage = e.options[e.selectedIndex].text;
+      this.selectedSourceLanguage = e.options[e.selectedIndex].value;
       this.translateText();
     },
     selectTargetLanguage() {
       const e = document.getElementById('selectedTargetLanguage');
-      this.selectedTargetLanguage = e.options[e.selectedIndex].text;
+      this.selectedTargetLanguage = e.options[e.selectedIndex].value;
       this.translateText();
     },
 
     detectSourceLanguage() {
+      this.translatedText = '';
       if (new Date() - this.lastCall < 1000) {
         return;
       }
@@ -83,35 +85,6 @@ export default {
       });
     },
 
-    getLanguagesList() {
-      const options = {
-        method: 'GET',
-        url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages',
-        headers: {
-          'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-          'X-RapidAPI-Key': '2501d822damsh1a5073c2a7f7b41p1aa7f6jsnbea5985bc7d0',
-        },
-      };
-
-      this.$axios.request(options).then((response) => {
-        this.languagesList = response.data.data.languages;
-        const selSource = document.getElementById('selectedSourceLanguage');
-        const selTarget = document.getElementById('selectedTargetLanguage');
-        this.languagesList.forEach((language) => {
-          const optSource = document.createElement('option');
-          optSource.value = this.languagesList.id;
-          optSource.textContent += language.language;
-          selSource.appendChild(optSource);
-          const optTarget = document.createElement('option');
-          optTarget.value = this.languagesList.id;
-          optTarget.textContent += language.language;
-          selTarget.appendChild(optTarget);
-        });
-      }).catch((error) => {
-        console.error(error);
-      });
-    },
-
     autoDetectLanguage() {
       const encodedParams = new URLSearchParams();
       encodedParams.append('q', this.sourceText);
@@ -142,7 +115,64 @@ export default {
   },
   data() {
     return {
-      languagesList: [],
+      languagesList: {
+        Ukrainian: 'uk',
+        Amharic: 'am',
+        Arabic: 'ar',
+        Basque: 'eu',
+        Bengali: 'bn',
+        'English UK': 'en-GB',
+        'Portuguese Brazil': 'pt-BR',
+        Bulgarian: 'bg',
+        Catalan: 'ca',
+        Cherokee: 'chr',
+        Croatian: 'hr',
+        Czech: 'cs',
+        Danish: 'da',
+        Dutch: 'nl',
+        'English US': 'en',
+        Estonian: 'et',
+        Filipino: 'fil',
+        Finnish: 'fi',
+        French: 'fr',
+        German: 'de',
+        Greek: 'el',
+        Gujarati: 'gu',
+        Hebrew: 'iw',
+        Hindi: 'hi',
+        Hungarian: 'hu',
+        Icelandic: 'is',
+        Indonesian: 'id',
+        Italian: 'it',
+        Japanese: 'ja',
+        Kannada: 'kn',
+        Korean: 'ko',
+        Latvian: 'lv',
+        Lithuanian: 'lt',
+        Malay: 'ms',
+        Malayalam: 'ml',
+        Marathi: 'mr',
+        Norwegian: 'no',
+        Polish: 'pl',
+        'Portuguese Portugal': 'pt-PT',
+        Romanian: 'ro',
+        Russian: 'ru',
+        Serbian: 'sr',
+        'Chinese PRC': 'zh-CN',
+        Slovak: 'sk',
+        Slovenian: 'sl',
+        Spanish: 'es',
+        Swahili: 'sw',
+        Swedish: 'sv',
+        Tamil: 'ta',
+        Telugu: 'te',
+        Thai: 'th',
+        'Chinese Taiwan': 'zh-TW',
+        Turkish: 'tr',
+        Urdu: 'ur',
+        Vietnamese: 'vi',
+        Welsh: 'cy',
+      },
       selectedSourceLanguage: '',
       selectedTargetLanguage: 'uk',
       translatedText: '',
@@ -172,11 +202,11 @@ export default {
 select {
   margin: 0 10px;
 }
-.translated-text-container{
+.source-text-container, .target-text-container{
   margin: 0 20px;
   padding: 5px;
-  width: 30vh;
-  height: 10vh;
-  border: 2px solid #2c3e50;
+  width: 50vh;
+  height: 15vh;
+  border: 1px solid #2c3e50;
 }
 </style>
